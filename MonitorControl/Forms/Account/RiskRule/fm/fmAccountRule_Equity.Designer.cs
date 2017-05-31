4552ba7e33f1b0bde0bfdac04426671be8c167b5
qianbo <qianbo@futsystems.com>
2017-05-31 13:11:01 +0800

    input value limit

--- MonitorControl/Forms/Account/RiskRule/fm/fmAccountRule_Equity.Designer.cs
+++ MonitorControl/Forms/Account/RiskRule/fm/fmAccountRule_Equity.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.lbDesp = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             this.btnSubmit = new ComponentFactory.Krypton.Toolkit.KryptonButton();
             this.equity_warn = new ComponentFactory.Krypton.Toolkit.KryptonNumericUpDown();
             this.kryptonLabel4 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
@@ -37,7 +38,6 @@
             this.kryptonLabel3 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.lbDesp = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
             this.kryptonPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -59,6 +59,14 @@
             this.kryptonPanel1.Size = new System.Drawing.Size(456, 149);
             this.kryptonPanel1.TabIndex = 0;
             // 
+            // lbDesp
+            // 
+            this.lbDesp.Location = new System.Drawing.Point(12, 73);
+            this.lbDesp.Name = "lbDesp";
+            this.lbDesp.Size = new System.Drawing.Size(20, 20);
+            this.lbDesp.TabIndex = 9;
+            this.lbDesp.Values.Text = "--";
+            // 
             // btnSubmit
             // 
             this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
@@ -73,19 +81,19 @@
             this.equity_warn.DecimalPlaces = 2;
             this.equity_warn.Location = new System.Drawing.Point(150, 34);
             this.equity_warn.Maximum = new decimal(new int[] {
-            10000000,
+            100000000,
             0,
             0,
             0});
             this.equity_warn.Name = "equity_warn";
-            this.equity_warn.Size = new System.Drawing.Size(99, 20);
+            this.equity_warn.Size = new System.Drawing.Size(99, 22);
             this.equity_warn.TabIndex = 7;
             // 
             // kryptonLabel4
             // 
             this.kryptonLabel4.Location = new System.Drawing.Point(255, 36);
             this.kryptonLabel4.Name = "kryptonLabel4";
-            this.kryptonLabel4.Size = new System.Drawing.Size(38, 18);
+            this.kryptonLabel4.Size = new System.Drawing.Size(35, 20);
             this.kryptonLabel4.TabIndex = 6;
             this.kryptonLabel4.Values.Text = "报警";
             // 
@@ -93,7 +101,7 @@
             // 
             this.kryptonLabel5.Location = new System.Drawing.Point(115, 36);
             this.kryptonLabel5.Name = "kryptonLabel5";
-            this.kryptonLabel5.Size = new System.Drawing.Size(38, 18);
+            this.kryptonLabel5.Size = new System.Drawing.Size(35, 20);
             this.kryptonLabel5.TabIndex = 5;
             this.kryptonLabel5.Values.Text = "低于";
             // 
@@ -102,19 +110,19 @@
             this.equity_flat.DecimalPlaces = 2;
             this.equity_flat.Location = new System.Drawing.Point(150, 10);
             this.equity_flat.Maximum = new decimal(new int[] {
-            10000000,
+            100000000,
             0,
             0,
             0});
             this.equity_flat.Name = "equity_flat";
-            this.equity_flat.Size = new System.Drawing.Size(99, 20);
+            this.equity_flat.Size = new System.Drawing.Size(99, 22);
             this.equity_flat.TabIndex = 4;
             // 
             // kryptonLabel3
             // 
             this.kryptonLabel3.Location = new System.Drawing.Point(255, 12);
             this.kryptonLabel3.Name = "kryptonLabel3";
-            this.kryptonLabel3.Size = new System.Drawing.Size(133, 18);
+            this.kryptonLabel3.Size = new System.Drawing.Size(123, 20);
             this.kryptonLabel3.TabIndex = 3;
             this.kryptonLabel3.Values.Text = "强平持仓并冻结帐户";
             // 
@@ -122,7 +130,7 @@
             // 
             this.kryptonLabel2.Location = new System.Drawing.Point(115, 12);
             this.kryptonLabel2.Name = "kryptonLabel2";
-            this.kryptonLabel2.Size = new System.Drawing.Size(38, 18);
+            this.kryptonLabel2.Size = new System.Drawing.Size(35, 20);
             this.kryptonLabel2.TabIndex = 1;
             this.kryptonLabel2.Values.Text = "低于";
             // 
@@ -130,17 +138,9 @@
             // 
             this.kryptonLabel1.Location = new System.Drawing.Point(12, 12);
             this.kryptonLabel1.Name = "kryptonLabel1";
-            this.kryptonLabel1.Size = new System.Drawing.Size(106, 18);
+            this.kryptonLabel1.Size = new System.Drawing.Size(98, 20);
             this.kryptonLabel1.TabIndex = 0;
             this.kryptonLabel1.Values.Text = "当交易帐户权益";
-            // 
-            // lbDesp
-            // 
-            this.lbDesp.Location = new System.Drawing.Point(12, 73);
-            this.lbDesp.Name = "lbDesp";
-            this.lbDesp.Size = new System.Drawing.Size(19, 18);
-            this.lbDesp.TabIndex = 9;
-            this.lbDesp.Values.Text = "--";
             // 
             // fmAccountRule_Equity
             // 
